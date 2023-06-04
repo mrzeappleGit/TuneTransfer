58a07ca6ef0ff94458ce5ecc711eac5de20ec9f6
Matthew Thomas Stevens <matthew.stevens@matthewstevens.me>
2023-06-04 10:48:26 -0500

    updated image

--- js/tunetransferMain.js
+++ js/tunetransferMain.js
@@ -4,6 +4,6 @@ window.onload = function() {
     tunetransferElement.id = "tunetransfer";
     tunetransferElement.className = "yt-uix-button yt-uix-button-size-default yt-uix-button-default";
     tunetransferElement.type = "button";
-    tunetransferElement.innerHTML = "<img src='https://raw.gi' id='tuneTransferImage' style='width: 20px; height: 20px; margin-right: 5px; margin-bottom: 2px;'>";
+    tunetransferElement.innerHTML = "<img src='https://raw.githubusercontent.com/mrzeappleGit/TuneTransfer/master/assets/spotify-logo.png' id='tuneTransferImage' style='width: 20px; height: 20px; margin-right: 5px; margin-bottom: 2px;'>";
     document.getElementById("subscribe-button").insertAdjacentElement("afterEnd", tunetransferElement);
 };
